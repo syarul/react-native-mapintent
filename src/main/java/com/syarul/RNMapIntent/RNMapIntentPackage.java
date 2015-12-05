22ac741956571ffb2201852498b63d5c5104dd2f
Tomas Sereikis <tsereikis@kenzanmedia.com>
2015-12-05 20:02:09 +0200

    fix for android package sctucture

--- src/main/java/com/syarul/RNMapIntent/RNMapIntentPackage.java
+++ src/main/java/com/syarul/RNMapIntent/RNMapIntentPackage.java
@@ -1,16 +1,13 @@
-package com.syarul.mapintent;
+package com.syarul.rnmapintent;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import com.syarul.mapintent.RNMapIntentModule;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.views.view.ReactViewManager;
 import com.facebook.react.uimanager.ViewManager;
 
 public class RNMapIntentPackage implements ReactPackage {
